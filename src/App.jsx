6a0d97f5288cf56bcdb769de573a677097da48ef
Manish <Manish.Kumar2@pepsico.com>
2024-05-24 21:10:37 +0530

    added courses in service

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import Footer from './Component/Footer'
+import Logo from './Component/Logo'
+import Navbar from './Component/Navbar'
+import {  BrowserRouter as Router,  Routes,  Route, useLocation} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CourseHome from './Component/allcourses/CourseHome'
+
+function AppContent() { 
+  const location = useLocation();
+
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/service' element={<CourseHome />} />
+      </Routes>
+      {location.pathname !== '/service' && <Logo />}
+      <Footer />
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
+}
+
+export default App;
